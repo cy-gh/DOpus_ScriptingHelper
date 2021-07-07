4700300617080e47695d33e6173dfb8b95c16fb7
cy-gh <github_20200914@cuneytyilmaz.com>
2021-07-08 01:34:14 +0200

    added .gitattributes

---
+++ index.ts
@@ -0,0 +1,6 @@
+// @ts-check
+/* global ActiveXObject Enumerator DOpus Script */
+///<reference path='./_DOpusDefinitions.d.ts' />
+
+// this file only demonstrates how to bind the definitions
+// in JScript/JavaScript or TypeScript files
